da17678b2ca62b9c6a7ba00737c1dca02fae3273
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-10-10 07:17:59 +0200

    comm

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
 import { errorHandler } from './middlewares/errorHandler.js';
-
 import routerContacts from './routers/contactsRouters.js';
-
 import env from './utils/env.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
-
 import authRouter from './routers/auth.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { UPLOAD_DIR } from './constants/index.js';
@@ -20,15 +16,19 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cookieParser());
 
+
+  app.use('/api-docs', swaggerDocs());
+
   app.use(authRouter);
   app.use(routerContacts);
+
+
   app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
+
   app.use(notFoundHandler);
 
   app.use(errorHandler);
 
   const port = Number(env('PORT', 3000));
-
   app.listen(port, () => console.log(`Server running on port ${port}`));
 };
